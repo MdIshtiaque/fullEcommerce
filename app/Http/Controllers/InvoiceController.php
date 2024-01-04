005434baa7b721d520693f8eacca9d26b31f5dc1
emon <ishtiaqueferdous109@gmail.com>
2024-01-04 16:31:32 +0600

    Invoice Show on order table Done

---
+++ app/Http/Controllers/InvoiceController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Order;
+use App\Models\OrderedProduct;
+use Illuminate\Http\Request;
+
+class InvoiceController extends Controller
+{
+    public function showInvoice(Order $order)
+    {
+        $order = $order->load('billingDetail');
+        $items = OrderedProduct::with('cart.product')->whereOrder_id($order->id)->get();
+        $categories = Category::all();
+
+        return view('pages.invoice.invoice', ['order' => $order, 'items' => $items, 'categories' => $categories]);
+    }
+}
