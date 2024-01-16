c5bb1a9c361f04f3ca4e5f1df76ee8bacfd67502
emon <ishtiaqueferdous109@gmail.com>
2024-01-16 18:18:17 +0600

    Custom Paginate Added

--- app/Http/Controllers/CategoryToProductController.php
+++ app/Http/Controllers/CategoryToProductController.php
@@ -10,14 +10,14 @@ use Illuminate\Http\Request;
 class CategoryToProductController extends Controller
 {
     public function showCategoryWiseProducts(Category $category) {
-        $itemsPerPage = 11;
+        $itemsPerPage = 16;
         $items = ProductCategory::with('product', 'product.productImage', 'product.symbol')->whereCategory_id($category->id)->paginate($itemsPerPage);
         $categories = Category::all();
         return view('pages.shop.products', ['items' => $items, 'categories' => $categories]);
     }
 
     public function showAllProducts() {
-        $itemsPerPage = 11;
+        $itemsPerPage = 16;
         $items = Product::with('productImage', 'symbol')->paginate($itemsPerPage);
         $categories = Category::all();
         return view('pages.shop.allProducts', ['items' => $items, 'categories' => $categories]);
